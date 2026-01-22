a1973967d4280d78af7fa5a6992229b38e47db03
Jonathan C. Dietrich <jcdietrich@gmail.com>
2026-01-22 16:32:14 -0500

    adding logging

--- custom_components/custom_event/__init__.py
+++ custom_components/custom_event/__init__.py
@@ -1,8 +1,10 @@
 from homeassistant.core import HomeAssistant, ServiceCall, SupportsResponse
 from homeassistant.helpers.typing import ConfigType
 import voluptuous as vol
+import logging
 
 DOMAIN = "custom_event"
+_LOGGER = logging.getLogger(__name__)
 
 CALL_SCHEMA = vol.Schema(
     {
@@ -16,6 +18,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType):
     async def handle_fire(call: ServiceCall):
         event_type = call.data.get("event_type")
         event_data = call.data.get("event_data")
+        _LOGGER.debug("Firing event %s with data: %s", event_type, event_data)
         hass.bus.async_fire(event_type, event_data)
         return {"event": event_type, "sent": True}
 
